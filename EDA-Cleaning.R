210bf4cf8246c626144d6983907090895b8c36cc
Shane Huang <shanehuang16@gmail.com>
2020-09-18 15:55:35 -0600

    Tidied up some stuff in the EDA file and created an initial model

--- EDA-Cleaning.R
+++ EDA-Cleaning.R
@@ -1,22 +1,39 @@
-##
-## 14-3-3 Phosphorylations
+###########################
+## EDA and Data Cleaning ##
+###########################
 
 ## Libraries
 library(tidyverse)
-library(caret)
 library(DataExplorer)
 
 ## Read files
 phos_train <- read_csv("train.csv")
 phos_test <- read_csv("test.csv")
 
-phos_data <- bind_rows(train=phos_train, test=phos_test, .id="Set")
+phos <- bind_rows(train=phos_train, test=phos_test, .id="Set")
 
 ## EDA
-summary(phos_data)
-plot_missing(phos_data)
-plot_correlation(phos_data, type="continuous", 
+summary(phos)
+plot_missing(phos)
+plot_correlation(phos, type="continuous", 
                  cor_args=list(use="pairwise.complete.obs"))
 
 # Change characters to factors
-phos_data <- phos_data %>% mutate_at(c('Set','Amino.Acid'), as.factor(.))
+phos <- phos %>% mutate_at(c('Set','Amino.Acid','Response'), funs(factor(.)))
+
+# Stochastic Imputation for PSSM
+pssm.lm <- lm(PSSM~Amino.Acid+Iupred.score+ANN+SVM+normalization, data=phos)
+pssm.preds <- round((predict(pssm.lm, newdata=(phos %>% filter(is.na(PSSM))))+
+                   rnorm(sum(is.na(phos$PSSM)), 0, sigma(pssm.lm)))^2,3)
+phos <- phos %>%
+  mutate(PSSM=replace(PSSM, is.na(PSSM), pssm.preds))
+
+rm(pssm.lm)
+
+# Compute missing Consensus using mean
+phos <- phos %>% mutate(Consensus=replace(Consensus, is.na(Consensus),
+                                              round(rowMeans(phos %>% filter(is.na(Consensus)) %>% select('ANN','PSSM','SVM')),3)))
+
+
+# Write out cleaned data
+write_csv(x=phos, path="./phos_cleaned.csv")
---
+++ Modelling.R
@@ -0,0 +1,46 @@
+###############
+## Modelling ##
+###############
+
+## Read in data
+phos <- read_csv('phos_cleaned.csv')
+
+## Libraries
+library(tidyverse)
+library(caret)
+library(DataExplorer)
+
+## Split train and test
+phos_train <- phos %>% filter(Set == 'train')
+phos_test <- phos %>% filter(Set == 'test')
+
+
+## XGBTree Model
+train.control=trainControl(method="repeatedcv", number=10, repeats=3,
+                       summaryFunction = prSummary)
+
+tunegrid = expand.grid(nrounds = seq(10,50,length.out = 4),
+                       max_depth = 1:5,
+                       eta = seq(0.1,0.4,0.1),
+                       gamma = 0,
+                       colsample_bytree = 0.6,
+                       min_child_weight = 1:3,
+                       subsample = 0.5)
+
+xgb_model <- train(form=Response~.,
+               data = (phos_train %>% select(-Set, -SiteNum)),
+               method = "xgbTree",
+               preProcess = c("center", "scale"),
+               metric = "F",
+               trControl = train.control,
+               tuneGrid = tunegrid
+               )
+beepr::beep(sound=5)
+
+xgb_model$bestTune
+print(xgb_model)
+plot(xgb_model)
+
+preds <- predict(xgb_model, phos_test)
+out <- data.frame(Id=phos_test$SiteNum, Predicted=preds)
+write_csv(out, "xgb-preds.csv")
