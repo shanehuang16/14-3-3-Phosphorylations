1ab82fdcfaffe6d1c6c4f860222f1e78f580390b
Connor <cgreen640@gmail.com>
2020-09-22 19:50:35 -0600

    Touched up the model

--- Phos_SVM_Connor.R
+++ Phos_SVM_Connor.R
@@ -8,7 +8,8 @@ library(caret)
 
 ## Read Data
 phos <- read_csv("phos_cleaned.csv")
-phos$Amino.Acid <- as.factor(phos$Response)
+phos$Response <- as.factor(phos$Response)
+
 
 ## Split into train and test
 phos_train <- phos %>% filter(Set == "train")
@@ -20,15 +21,16 @@ train_control <- trainControl(method = "repeatedcv",
                               repeats = 3,
                               summaryFunction = prSummary)
 
-tunegrid <- expand.grid(C = seq(.5, 3, length = 500))
+tunegrid <- expand.grid(C = seq(0, 3, length = 40))
 
+##
 tictoc::tic()
 phos_svm <- train(form=Response~.,
                   data = (phos_train %>% select(-Set, -SiteNum)),
                   method = "svmLinear",
                   preProcess = c("center", "scale", "pca"),
                   metric = "F",
-                  trControl = train.control,
+                  trControl = train_control,
                   tuneGrid = tunegrid
 )
 tictoc::toc()
@@ -37,3 +39,7 @@ beepr::beep(sound=1)
 phos_svm$bestTune
 print(phos_svm)
 plot(phos_svm)
+
+preds_svm <- phos_svm %>% predict(phos_test) 
+svm_preds1<- data.frame(Id = phos_test$SiteNum, Predicted = preds_svm)
+write.csv(svm_preds1,"C:\\Users\\18592\\Documents\\Kaggle\\Phosphorylations_Team\\14-3-3-Phosphorylations\\SVM1 Predictions.csv", row.names = FALSE)
